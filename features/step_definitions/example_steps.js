88de30e2db05c47990d45b07172bd954e4b79f5d
iskomt <iskomt@yahoo.com>
2019-06-14 22:07:33 -0500

    initial commit

---
+++ features/step_definitions/example_steps.js
@@ -0,0 +1,16 @@
+const { Given, When, Then } = require('cucumber');
+
+Given('A page', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('More is clicked', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('Go to about page', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
---
+++ features/test.feature
@@ -0,0 +1,10 @@
+
+Feature: Navigate to about page
+	In order to navigate to about page
+	As a guest
+	I want to be able to navigate to about page
+
+Scenario: Successfully navigate to next page
+    Given A page
+    When More is clicked
+    Then Go to about page
